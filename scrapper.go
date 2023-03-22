490a6fddb7fa8e1730112f7c4e9fc8b6b9d97a7f
divyansh-sharma <dsharma2@chirpn.com>
2023-03-23 03:01:42 +0530

    fixed url call

--- scrapper.go
+++ scrapper.go
@@ -1,73 +1,75 @@
-package main 
- 
-import ( 
+package main
+
+import (
+	"encoding/csv"
 	"fmt"
-	"encoding/csv" 
-	"github.com/gocolly/colly" 
-	"log" 
-	"os" 
-) 
- 
-// initializing a data structure to keep the scraped data 
+	"log"
+	"os"
+
+	"github.com/gocolly/colly"
+)
+
+// initializing a data structure to keep the scraped data
 type BlogPost struct { 
-	url, image, name string 
+    url, name string 
 } 
  
 func main() { 
-	// initializing the slice of structs to store the data to scrape 
-	var blogs []BlogPost
+    fmt.Println("hii everyone....")
+    // initializing the slice of structs to store the data to scrape 
+    var blogs []BlogPost
  
-	// creating a new Colly instance 
-	c := colly.NewCollector() 
+    // creating a new Colly instance 
+    c := colly.NewCollector() 
  
-	// visiting the target page 
-	c.Visit("https://www.arkoselabs.com/blog/") 
+ 
+    // scraping logic 
+    c.OnHTML("div.al-post-item", func(e *colly.HTMLElement) { 
+        blog := BlogPost{} 
+ 
+        blog.url = e.ChildAttr("a", "href")  
+        blog.name = e.ChildText(".auth-data") 
+ 
+        blogs = append(blogs, blog) 
+        fmt.Println("hi there/./././../.")
+    }) 
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+        fmt.Println("Visiting", r.URL)
+    })
+
+	// visiting the target page 
+    c.Visit("https://www.arkoselabs.com/blog/") 
+
  
-	// scraping logic 
-	c.OnHTML("div.al-post-item", func(e *colly.HTMLElement) { 
-		blog := BlogPost{} 
+    // opening the CSV file 
+    file, err := os.Create("blogs.csv") 
+    if err != nil { 
+        log.Fatalln("Failed to create output CSV file", err) 
+    } 
+    defer file.Close() 
  
-		blog.url = e.ChildAttr("a", "href") 
-		blog.image = e.ChildAttr("img", "src") 
-		blog.name = e.ChildText(".auth-data") 
+    // initializing a file writer 
+    writer := csv.NewWriter(file) 
  
-		blogs = append(blogs, blog) 
-		fmt.Println("hi there/./././../.")
-	}) 
+    // writing the CSV headers 
+    headers := []string{ 
+        "url", 
+        "name",  
+    } 
+    writer.Write(headers) 
  
-	// opening the CSV file 
-	file, err := os.Create("blogs.csv") 
-	if err != nil { 
-		log.Fatalln("Failed to create output CSV file", err) 
-	} 
-	defer file.Close() 
+    // writing each blog as a CSV row 
+    for _, blog := range blogs { 
+        // converting a blog to an array of strings 
+        record := []string{ 
+            blog.url, 
+            blog.name, 
+        } 
  
-	// initializing a file writer 
-	writer := csv.NewWriter(file) 
- 
-	// writing the CSV headers 
-	headers := []string{ 
-		"url", 
-		"image", 
-		"name",  
-	} 
-	writer.Write(headers) 
- 
-	// writing each blog as a CSV row 
-	for _, blog := range blogs { 
-		// converting a blog to an array of strings 
-		record := []string{ 
-			blog.url, 
-			blog.image, 
-			blog.name, 
-		} 
- 
-		// adding a CSV record to the output file 
-		writer.Write(record) 
-	} 
-	defer writer.Flush() 
+        // adding a CSV record to the output file 
+        writer.Write(record) 
+    } 
+    defer writer.Flush() 
+	
 }
